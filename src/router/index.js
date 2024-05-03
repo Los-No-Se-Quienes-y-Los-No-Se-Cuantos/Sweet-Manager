c8710d132227678a7405a95e58ad3dda28ebdde9
Ryzeon <alex.ramon2004@gmail.com>
2024-05-03 04:36:06 -0500

    feat(login): init route

--- src/router/index.js
+++ src/router/index.js
@@ -4,15 +4,25 @@ import ReportsViewComponent from "../analytics/components/reports-view/reports-v
 import NotificationsContentComponent from "../interaction/components/notifications-content.component.vue";
 import CompanyPage from "../profiles/pages/company-page.vue";
 import SubscriptionsContentComponent from "../payment/pages/subscriptions-content.component.vue";
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: SubscriptionsContentComponent, meta: { title: 'Login'}},
-        { path: '/dashboard', component: ReportsViewComponent, meta: { title: 'Reports'}},
-
-        // { path: '/tutorials', component: TutorialManagementComponent, meta: { title: 'Tutorials' } },
-        // { path: '/about', component: AboutComponent, meta: { title: 'About us'}},
+        {path: '/', component: SubscriptionsContentComponent,},
+        {path: '/dashboard', component: ReportsViewComponent, meta: {requiresAuth: true}},
     ]
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth) {
+        const token = localStorage.getItem('id');
+
+        // this route requires auth, check if logged in
+        // if not, redirect to login page.
+        next({path: '/'});
+    } else {
+        next(); // make sure to always call next()!
+    }
+})
 
 export default router;
