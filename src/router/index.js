91a1de1239d8c9a6a69524947e5e961ed865b383
Ryzeon <alex.ramon2004@gmail.com>
2024-05-03 04:26:31 -0500

    feat(suscription): under develop?

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,11 @@ import AccessContentComponent from "../iam/components/access-view/access-content
 import ReportsViewComponent from "../analytics/components/reports-view/reports-view.component.vue";
 import NotificationsContentComponent from "../interaction/components/notifications-content.component.vue";
 import CompanyPage from "../profiles/pages/company-page.vue";
+import SubscriptionsContentComponent from "../payment/pages/subscriptions-content.component.vue";
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: AccessContentComponent, meta: { title: 'Login'}},
+        { path: '/', component: SubscriptionsContentComponent, meta: { title: 'Login'}},
         { path: '/dashboard', component: ReportsViewComponent, meta: { title: 'Reports'}},
 
         // { path: '/tutorials', component: TutorialManagementComponent, meta: { title: 'Tutorials' } },
