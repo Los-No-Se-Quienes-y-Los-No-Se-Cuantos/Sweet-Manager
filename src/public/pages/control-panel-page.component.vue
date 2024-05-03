3c02d01ffdda81b22bb640d946fa9452854bf98b
Ryzeon <alex.ramon2004@gmail.com>
2024-05-03 06:29:48 -0500

    feat(routing): routing.

--- src/public/pages/control-panel-page.component.vue
+++ src/public/pages/control-panel-page.component.vue
@@ -1,11 +1,16 @@
 <script>
+import ReportsView from "../../analytics/components/reports-view/reports-view.component.vue";
+import NotificationsContent from "../../interaction/components/notifications-content.component.vue";
+
 export default {
-  name: "control-panel-page"
+  name: "control-panel-page",
+  components: {NotificationsContent, ReportsView}
 }
 </script>
 
 <template>
-
+  <reports-view/>
+  <notifications-content/>
 </template>
 
 <style scoped>
